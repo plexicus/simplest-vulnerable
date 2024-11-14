c92c7c7b1fbbb593f1f512933f4da05adfa6df60
plexicus <plexicus@plexicus.com>
2024-11-14 03:30:14 +0000

    Cross-site Scripting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from markupsafe import escape
 
 app = FastAPI()
 
@@ -9,6 +10,6 @@ async def read_user_input(user_input: str):
     # Esta línea introduce una vulnerabilidad XSS porque el input del usuario se
     # devuelve directamente en la respuesta sin ninguna sanitización o escape.
     # Un atacante podría ingresar un script como parte de user_input.
-    return f"<html><body><h1>Your input was: {user_input}</h1></body></html>"
-
+    safe_input = escape(user_input)
+    return f"<html><body><h1>Your input was: {safe_input}</h1></body></html>"
 
