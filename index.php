36762b531cc876abd04d4e8eebac711f99ac6866
PlexicusAI <info@plexicus.com>
2025-08-22 09:47:33 +0000

    Cross-site Scripting

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if(isset($_GET['id'])) {
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"]. " - Nombre: " . $row["nombre"]. "<br>";
+    echo "<div>" . htmlentities($mensaje, ENT_QUOTES, 'UTF-8') . "</div>";
         }
     } else {
         echo "0 resultados";
@@ -34,8 +34,9 @@ if(isset($_GET['id'])) {
 if(isset($_GET['mensaje'])) {
     $mensaje = $_GET['mensaje']; // Input del usuario susceptible a XSS
     echo "<div>$mensaje</div>"; // Vulnerable a XSS
-}
-
-// Cerrar conexión
+    }
+    else {
+    echo "<div>" . htmlentities($mensaje, ENT_QUOTES, 'UTF-8') . "</div>";
+    }
 $conn->close();
 ?>
